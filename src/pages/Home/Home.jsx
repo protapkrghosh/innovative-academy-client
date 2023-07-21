92712e39a1f6433129b923233e0e120757e433d7
Protap Ghosh <protapg518@gmail.com>
2023-07-22 02:58:30 +0600

    route setup

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,15 @@
+import { Helmet } from "react-helmet-async";
+
+const Home = () => {
+   return (
+      <div>
+         <Helmet>
+            <title>Innovative Collage</title>
+         </Helmet>
+
+         
+      </div>
+   );
+};
+
+export default Home;
